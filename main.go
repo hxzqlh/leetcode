2909b4adac75ef75aa7569b0ecb9140b5cfaf681
hxzqlh <hxzqlh@gmai.com>
2021-09-14 00:01:56 +0800

    job

--- main.go
+++ main.go
@@ -1,13 +1,99 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-
-	"github.com/hxzqlh/leetcode/sorting"
+	"runtime"
+	"time"
 )
 
+const (
+	N       = 5
+	ResOk   = 0
+	ResFail = 1
+)
+
+type JobRes struct {
+	job  string
+	code int
+	err  error
+}
+
+var APIList = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+
+// 最多 N 个并发
+// 执行一系列任务
+// 任一任务有错误，立即返回
+// 等待所有任务执行完成
 func main() {
-	a := []int{1, 4, 7, 2, 5, 6, 3, 6, 5}
-	sorting.QuickSort(a, 0, len(a)-1)
-	fmt.Println(a)
+	workerCh := make(chan struct{}, N)
+	for i := 0; i < N; i++ {
+		workerCh <- struct{}{}
+	}
+
+	resCh := make(chan JobRes, len(APIList))
+	waitCh := make(chan struct{})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		var num int
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Printf("res goroutine canceled by main, reason:%s\n", ctx.Err())
+				return
+			case res := <-resCh:
+				num++
+				fmt.Printf("job:%v return err:%v\n", res.job, res.err)
+				if num == len(APIList) || res.code != ResOk{
+					cancel()
+					waitCh <- struct{}{}
+				}
+			}
+		}
+	}()
+
+	for _, job := range APIList {
+		// wait worker available
+		<-workerCh
+		go workerFn(ctx, workerCh, resCh, job)
+	}
+
+	<-waitCh
+	time.Sleep(time.Second)
+	fmt.Println("main exit")
+}
+
+func workerFn(ctx context.Context, workerCh chan struct{}, resCh chan JobRes, job string) {
+	fmt.Println("goroutines:", runtime.NumGoroutine())
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("job:%s canceled by main, reason:%s\n", job, ctx.Err())
+			return
+		default:
+			code := ResOk
+			err := callApi(job)
+			if err != nil {
+				code = ResFail
+			}
+
+			resCh <- JobRes{job: job, code: code, err: err}
+
+			// reuse worker
+			workerCh <- struct{}{}
+			return
+		}
+	}
+}
+
+func callApi(a string) error {
+	// handler
+	time.Sleep(200 * time.Millisecond)
+	if a == "e" {
+		return errors.New(a)
+	}
+	return nil
 }
